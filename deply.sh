ab2f70a2f4819ef5d6bfc372d2972ad47881470a
Carrick7 <92592671+Carrick7@users.noreply.github.com>
2022-03-24 13:32:39 +0000

    updating yml file and created deploy.sh

---
+++ deply.sh
@@ -0,0 +1,14 @@
+# !/usr/bin/env bash
+sudo apt update && sudo apt install nodejs npm
+# Install pm2 which is a production process manager for Node.js witha built-in load balancer
+sudo npm install -g pm2
+# stop any instance of our application running currently
+pm2 stop SimpleApplication
+# change directory into folder where application is downloaded
+cd SimpleApplication/
+# Install application dependencies
+npm install
+# pm2 save error
+sudo pm2 save --force
+# Start the applciation with the process name SimpleApplication using pm2
+pm2 start ./bin/www --name SimpleApplication
