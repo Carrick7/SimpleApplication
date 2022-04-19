d7375c1c655fefbdd63708e8fd17bb007e0b9a87
Carrick7 <92592671+Carrick7@users.noreply.github.com>
2022-04-19 15:52:15 +0100

    troubleshooting docker integration

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,32 @@
 # !/usr/bin/env bash
-sudo apt update && sudo apt install nodejs npm
-# Install pm2 which is a production process manager for Node.js witha built-in load balancer
-sudo npm install -g pm2
-# stop any instance of our application running currently
-pm2 stop SimpleApplication
-# change directory into folder where application is downloaded
-cd SimpleApplication/
-# Install application dependencies
-npm install
+# Check if there is instance running with the image name we are deploying
+CURRENT_INSTANCE=$(docker ps -a -q --filter ancestor="$IMAGE_NAME" --format="{{.ID}}")
+
+#if an instance does exist, stop the instance
+if [ "$CURRENT_INSTANCE" ]
+then
+  docker rm $(docker stop $CURRENT_INSTANCE)
+fi
+
+# pull down the instance from dockerhub
+docker pull $IMAGE_NAME
+
+#check if a docker container exists with the name of node_app, remove if it does.
+CONTAINER_EXISTS=$(docker ps -a | grep node_app)
+if [ "$CONTAINER_EXISTS" ]
+then  
+  docker rm node_app
+fi
+
+#create container called node_app that is availible on port 8443 from our docker image
+docker create -p 8443:8443 --name node_app $IMAGE_NAME
+#write the private key to a file 
 echo $privatekey > privatekey.pem
+#write the server key to a file 
 echo $server > server.crt
-# pm2 save error
-sudo pm2 save --force
-# Start the applciation with the process name SimpleApplication using pm2
-pm2 start ./bin/www --name SimpleApplication
+#Add the private key to the node_app docker container
+docker cp ./privatekey.pem node_app:/privatekey.pem
+#Add the server key to the node_app docker container
+docker cp ./server.crt node_app:/server.crt
+#start the node_app container
+docker start node_app
